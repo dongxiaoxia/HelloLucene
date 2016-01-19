00f69eabe515a2b2c46a537a9365199ceac3da52
dongxiaoxia <810196858@qq.com>
2016-01-20 00:18:23 +0800

    new file:   src/main/java/xyz/dongxiaoxia/hellolucene/util/HighlighterParam.java 	new file:   src/main/java/xyz/dongxiaoxia/hellolucene/util/LuceneManager.java 	new file:   src/main/java/xyz/dongxiaoxia/hellolucene/util/LuceneUtils.java 	new file:   src/main/java/xyz/dongxiaoxia/hellolucene/util/Page.java 	modified:   .idea/workspace.xml 	modified:   src/main/java/xyz/dongxiaoxia/hellolucene/util/HighlighterParam.java 	modified:   src/main/java/xyz/dongxiaoxia/hellolucene/util/LuceneManager.java 	modified:   src/main/java/xyz/dongxiaoxia/hellolucene/util/LuceneUtils.java 	modified:   src/main/java/xyz/dongxiaoxia/hellolucene/util/Page.java 	modified:   target/HelloLucene.war 	target/HelloLucene/WEB-INF/classes/xyz/dongxiaoxia/hellolucene/util/ 	target/classes/xyz/dongxiaoxia/hellolucene/util/

---
+++ src/main/java/xyz/dongxiaoxia/hellolucene/util/HighlighterParam.java
@@ -0,0 +1,7 @@
+package xyz.dongxiaoxia.hellolucene.util;
+
+/**
+ * Created by dongxiaoxia on 2016/1/20.
+ */
+public class HighlighterParam {
+}
---
+++ src/main/java/xyz/dongxiaoxia/hellolucene/util/LuceneManager.java
@@ -0,0 +1,7 @@
+package xyz.dongxiaoxia.hellolucene.util;
+
+/**
+ * Created by dongxiaoxia on 2016/1/19.
+ */
+public class LuceneManager {
+}
